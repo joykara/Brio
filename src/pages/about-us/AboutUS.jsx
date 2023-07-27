c0640ccd32cd63919266ad0d24b31d01aa6927e0
joykara <joykarani1@gmail.com>
2023-07-27 10:18:12 +0300

    add About and Process pages

--- src/pages/about-us/AboutUS.jsx
+++ src/pages/about-us/AboutUS.jsx
@@ -8,6 +8,38 @@ const AboutUS = () => {
         <Navbar />
         <HeroHeader title={'ABOUT US'} subtitle={'OUR STORY'} description={'BrIO is a React UI Library that helps you build fast, accessible, and beautiful user interfaces for your web applications.'} />
         <div className="br-about-us">
+          <div className="br-about-sections">
+            <div className="br-about-section">
+              <div className="br-about-section-title">
+                <h1>OUR VISION</h1>
+              </div>
+              <div className="br-about-section-description">
+                <p>"To provide the best possible quality of life for our clients and community by
+                  delivering the highest standard of professional care."</p>
+                  <span></span>
+              </div>
+            </div>
+            <div className="br-about-section">
+              <div className="br-about-section-title">
+                <h1>OUR MISSION</h1>
+              </div>
+              <div className="br-about-section-description">
+                <p>"To provide the best possible quality of life for our clients and community by
+                  delivering the highest standard of professional care."</p>
+                  <span></span>
+              </div>
+            </div>
+            <div className="br-about-section">
+              <div className="br-about-section-title">
+                <h1>OUR VALUES</h1>
+              </div>
+              <div className="br-about-section-description">
+                <p>"To provide the best possible quality of life for our clients and community by
+                  delivering the highest standard of professional care."</p>
+                  <span></span>
+              </div>
+            </div>
+          </div>
         </div>
         <Footer />
     </>
--- src/pages/our-process/OurProcess.jsx
+++ src/pages/our-process/OurProcess.jsx
@@ -8,6 +8,35 @@ const OurProcess = () => {
         <Navbar />
         <HeroHeader title={'OUR PROCESS'} subtitle={'OUR PROCESS'} description={'BrIO is a React UI Library that helps you build fast, accessible, and beautiful user interfaces for your web applications.'} />
         <div className="br-our-process">
+          <div className="br-our-process-sections">
+            <div className="br-process-section">
+              <div className="br-process-section-title">
+                <h3>PROCESS</h3>
+              </div>
+              <div className="br-process-section-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet viverra nulla. Donec bibendum scelerisque ex, a condimentum augue mollis eget. In mattis turpis nisl, in fringilla tortor pellentesque a.</p>
+                  <span></span>
+              </div>
+            </div>
+            <div className="br-process-section">
+              <div className="br-process-section-title">
+                <h3>PROCESS</h3>
+              </div>
+              <div className="br-process-section-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet viverra nulla. Donec bibendum scelerisque ex, a condimentum augue mollis eget. In mattis turpis nisl, in fringilla tortor pellentesque a.</p>
+                  <span></span>
+              </div>
+            </div>
+            <div className="br-process-section">
+              <div className="br-process-section-title">
+                <h3>PROCESS</h3>
+              </div>
+              <div className="br-process-section-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet viverra nulla. Donec bibendum scelerisque ex, a condimentum augue mollis eget. In mattis turpis nisl, in fringilla tortor pellentesque a.</p>
+                  <span></span>
+              </div>
+            </div>
+          </div>
         </div>
         <Footer />
     </>
